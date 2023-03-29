c0880b36439f39baf213e6eb8edc1202d7672e03
Jignesh <jigsvadiyatar6557@gmail.com>
2023-03-29 22:32:26 +0530

    navbar completed

---
+++ Frontend/scripts/index.js
@@ -0,0 +1,13 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId)
+    const nav = document.getElementById(navId)
+
+
+    toggle.addEventListener("click", ()=>{
+        nav.classList.toggle("show-menu")
+
+        toggle.classList.toggle("show-icon")
+    })
+}
+
+showMenu("nav-toggle", "nav-menu") 
